e8db25fce1aa21884cd5b67a35fd78f53a0bce65
chriswynns1 <christopher.wynns.521@my.csun.edu>
2023-11-10 00:12:25 -0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './SignIn';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className=''>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div className='text-4xl'>
+      Sign In
+    </div>
+  )
+}
+
+export default SignIn
